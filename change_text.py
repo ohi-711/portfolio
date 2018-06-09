fbdfadd455c45e1ce9feb1de8c0d33a30a2fcbaa
billyhsun <39208126+billyhsun@users.noreply.github.com>
2018-06-09 18:58:50 -0400

    1.1.0

--- change_text.py
+++ change_text.py
@@ -24,10 +24,10 @@ def replace_text(filename, original_text, replaced_text):
 for filename in os.listdir(os.getcwd()):
     if filename.endswith(".html"): 
         print(filename)
-        replace_text(filename, "Earth Sciences Research", "Future Engineers Society") # Replace text
+        replace_text(filename, "<a href=\"index.html\"><img src=\"github.png\" alt=\"gh\" width=30></a>", "<a href=\"https://github.com/billyhsun?tab=overview&from=2018-05-01&to=2018-05-31\"><img src=\"github.png\" alt=\"gh\" width=30></a>") # Replace text
         continue
     else:
         continue        
-
+print ("Done")
 
 
